77c3179dd0c2ce0590e66ffec3a9b5a5ccd8b062
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 12:11:40 +0300

    Update A.cpp

--- Lab1/C++/A.cpp
+++ Lab1/C++/A.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+vector<int> SelectionSort(vector<int> A) {
+    int n = A.size();
+    for (int i = 0; i < n; i++) {
+        int maxIndex = i;
+        for (int j = i; j < n; j++) {
+            if (A[j] > A[maxIndex]) {
+                maxIndex = j;
+            }
+        }
+        swap(A[i], A[maxIndex]);
+    }
+    return A;
+}
+
+int main() {
+    vector<int> A;
+    int x;
+    while (cin >> x) A.push_back(x);
+
+    A = SelectionSort(A);
+
+    for (int i = 0; i < (int)A.size(); i++)
+        cout << A[i] << (i + 1 < (int)A.size() ? " " : "\n");
+
+    return 0;
+}
