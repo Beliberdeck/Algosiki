939c0975517a91d16de8681d017f617d3d2c5a3b
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 15:49:29 +0300

    Update H.py

--- Lab1/Python/H.py
+++ Lab1/Python/H.py
@@ -5,12 +5,9 @@ class Point:
 
 n = int(input())
 points = []
-
 for _ in range(n):
     x, y = map(int, input().split())
     points.append(Point(x, y))
-
 points.sort(key=lambda p: p.x ** 2 + p.y ** 2)
-
 for p in points:
     print(p.x, p.y)
