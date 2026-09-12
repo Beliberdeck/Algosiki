66d739eb161087b77163153aeada087801ea596f
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 15:48:08 +0300

    Update G.cpp

--- Lab1/C++/G.cpp
+++ Lab1/C++/G.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 void CountSort(vector<int>& A) {
     vector<int> count(101, 0);
-
     for (int x : A) {
         count[x]++;
     }
-
     int idx = 0;
     for (int value = 0; value <= 100; value++) {
         for (int c = 0; c < count[value]; c++) {
@@ -22,11 +20,8 @@ int main() {
     vector<int> A;
     int x;
     while (cin >> x) A.push_back(x);
-
     CountSort(A);
-
     for (int i = 0; i < (int)A.size(); i++)
         cout << A[i] << (i + 1 < (int)A.size() ? " " : "\n");
-
     return 0;
 }
