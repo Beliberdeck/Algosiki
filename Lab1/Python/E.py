aeb3e08c0d9d74a0a7c71c1144696d23bc76310c
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 14:34:28 +0300

    Update E.py

--- Lab1/Python/E.py
+++ Lab1/Python/E.py
@@ -1,1 +1,28 @@
+def merge_sort(A):
+    if len(A) <= 1:
+        return A
 
+    mid = len(A) // 2
+    left = merge_sort(A[:mid])
+    right = merge_sort(A[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+n = int(input())
+A = list(map(int, input().split()))
+print(*merge_sort(A))
