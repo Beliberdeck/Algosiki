cb345e34c10a4fab312393508ff16d98f7756c2f
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 15:47:25 +0300

    Update I.py

--- Lab1/Python/I.py
+++ Lab1/Python/I.py
@@ -1,1 +1,15 @@
+def anargram(a, b):
+    if len(a) != len(b):
+        return "NO"
+    count = {}
+    for x in a:
+        count[x] = count.get(x, 0) + 1
+    for x in b:
+        if x not in count or count[x] == 0:
+            return "NO"
+        count[x] -= 1
+    return "YES"
 
+a = input()
+b = input()
+print(anargram(a, b))
