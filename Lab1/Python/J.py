3cd7fd2d4248065e4b4a4439ed58e3b79f6d149a
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 15:55:50 +0300

    Update J.py

--- Lab1/Python/J.py
+++ Lab1/Python/J.py
@@ -1,1 +1,19 @@
+def compare(a, b):
+    return a + b > b + a
 
+
+pieces = []
+while True:
+    try:
+        pieces.append(input())
+    except EOFError:
+        break
+      
+for i in range(1, len(pieces)):
+    current = pieces[i]
+    j = i - 1
+    while j >= 0 and not compare(pieces[j], current):
+        pieces[j + 1] = pieces[j]
+        j -= 1
+    pieces[j + 1] = current
+print("".join(pieces))
