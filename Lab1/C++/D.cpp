77c8f6f0047bae6cd03b779b45f0aee58d29ffcc
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 14:07:37 +0300

    Update D.cpp

--- Lab1/C++/D.cpp
+++ Lab1/C++/D.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int BubbleSort(vector<int> a) {
+    int n = a.size();
+    int swaps = 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                swaps++;
+            }
+        }
+    }
+    return swaps;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    cout << BubbleSort(a) << endl;
+
+    return 0;
+}
