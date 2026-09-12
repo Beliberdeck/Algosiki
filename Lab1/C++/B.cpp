7c3033f38b1563d7cca3fec9d003f97e52a02efc
Beliberdeck <p.shtifanov@gmail.com>
2026-09-12 14:36:30 +0300

    Update B.cpp

--- Lab1/C++/B.cpp
+++ Lab1/C++/B.cpp
@@ -2,30 +2,30 @@
 #include <vector>
 using namespace std;
 
-vector<int> insertionSort(vector<int> v) {
-    int n = v.size();
+vector<int> insertionSort(vector<int> A) {
+    int n = A.size();
     for (int i = 1; i < n; i++) {
-        int val = v[i];
+        int val = A[i];
         int k = i - 1;
-        while (k >= 0 && v[k] > val) {
-            v[k + 1] = v[k];
+        while (k >= 0 && A[k] > val) {
+            A[k + 1] = A[k];
             k--;
         }
-        v[k + 1] = val;
+        A[k + 1] = val;
     }
-    return v;
+    return A;
 }
 
 int main() {
-    vector<int> v;
+    vector<int> A;
     int x;
     while (cin >> x) {
-        v.push_back(x);
+        A.push_back(x);
     }
-    v = insertionSort(v);
-    for (size_t i = 0; i < v.size(); i++) {
-        cout << v[i];
-        if (i + 1 < v.size()) {
+    A = insertionSort(A);
+    for (size_t i = 0; i < A.size(); i++) {
+        cout << A[i];
+        if (i + 1 < A.size()) {
             cout << " ";
         }
     }
