3592e9baeb7cdf6cd7340cb36e78cb3576f0065d
Beliberdeck <p.shtifanov@gmail.com>
2026-09-24 16:36:15 +0300

    Update B.py

--- Lab2/Python/B.py
+++ Lab2/Python/B.py
@@ -1,1 +1,19 @@
+def closest(arr, x):
+    left, right = 0, len(arr) - 1
+    while right - left > 1:
+        mid = (left + right) // 2
+        if arr[mid] < x:
+            left = mid
+        else:
+            right = mid
+    if x - arr[left] <= arr[right] - x:
+        return arr[left]
+    return arr[right]
 
+
+n, k = map(int, input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+for x in arr2:
+    print(closest(arr1, x))
