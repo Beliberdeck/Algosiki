611a23ba16329d1dde692d13445f021655444a9a
Beliberdeck <p.shtifanov@gmail.com>
2026-09-24 16:45:09 +0300

    Update D.py

--- Lab2/Python/D.py
+++ Lab2/Python/D.py
@@ -1,1 +1,9 @@
-
+C = float(input())
+left, right = 0.0, 100000.0
+for _ in range(100):
+    mid = (left + right) / 2
+    if mid ** 2 + mid ** 0.5 < C:
+        left = mid
+    else:
+        right = mid
+print(f"{right:.6f}")
